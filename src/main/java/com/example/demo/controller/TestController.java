0f8c1c9649633cc5b42ab56ea2a5b7a5cf4203e3
Choi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-06-10 19:25:10 +0900

    백엔드 개발 15.  @Service 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
+import com.example.demo.service.TodoService;
 
 @RestController
-@RequestMapping("test")
+@RequestMapping("todo")
 public class TestController {
+	
+	@Autowired
+	private TodoService service;
 
 	@GetMapping
 	public String testController() {
@@ -73,5 +78,17 @@ public class TestController {
 									.build();
 	
 	return ResponseEntity.badRequest().body(response);
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testTodo(){
+		String str = service.testService();
+		List<String> list = new ArrayList<>();
+		list.add(str);
+		
+		ResponseDTO<String> responseDTO = ResponseDTO.<String>builder()
+											.data(list).build();
+		
+		return ResponseEntity.ok().body(responseDTO);
 	}
 }
